5a23c845b62e278cca8a9b3b041e565c93f50353
SungJun Hong <castle_jun@daum.net>
2026-01-22 13:18:57 +0900

    Add ClientCore & Entities

--- Packages/Platform/Package.swift
+++ Packages/Platform/Package.swift
@@ -8,15 +8,29 @@ let package = Package(
     .macOS(.v15),
   ],
   products: [
+    .singleTargetLibrary("ClientCore"),
     .singleTargetLibrary("FeatureCore"),
   ],
   dependencies: [
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", exact: "1.10.1"),
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.23.1"),
   ],
   targets: [
     .target(
+      name: "ClientCore",
+      dependencies: [
+        "Entities",
+        .product(name: "Dependencies", package: "swift-dependencies"),
+        .product(name: "DependenciesMacros", package: "swift-dependencies"),
+      ]
+    ),
+    .target(
+      name: "Entities"
+    ),
+    .target(
       name: "FeatureCore",
       dependencies: [
+        "Entities",
         .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
       ]
     ),
